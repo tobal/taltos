292794aa5dd11f7ce5866f59f50b4945603d6c94
Tobal <tobal17@gmail.com>
2011-10-27 21:57:02 +0000

    Menu eventloop refactor

--- src/CommonModules/Menu.py
+++ src/CommonModules/Menu.py
@@ -19,16 +19,51 @@ class Menu(object):
         self.textDrawer = TextDrawer.TextDrawer()
         self.textGetter = TextGetter.TextGetter()
     
+    def menuUp(self, langChooserAlive, chosenLanguage):
+        if langChooserAlive:
+            if chosenLanguage == Languages.EN:
+                self.chosenLanguage = Languages.HU
+            elif chosenLanguage == Languages.HU:
+                self.chosenLanguage = Languages.ROV
+            elif chosenLanguage == Languages.ROV:
+                self.chosenLanguage = Languages.EN
+    
+    def menuDown(self, langChooserAlive, chosenLanguage):
+        if langChooserAlive:
+            if chosenLanguage == Languages.EN:
+                self.chosenLanguage = Languages.ROV
+            elif chosenLanguage == Languages.HU:
+                self.chosenLanguage = Languages.EN
+            elif chosenLanguage == Languages.ROV:
+                self.chosenLanguage = Languages.HU
+    
+    def eventLoop(self, langChooserAlive, chosenLanguage):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+            if event.type == KEYDOWN:
+                if event.key == K_w or event.key == K_UP:
+                    self.menuUp(langChooserAlive, chosenLanguage)
+                if event.key == K_s or event.key == K_DOWN:
+                    self.menuDown(langChooserAlive, chosenLanguage)
+                if (event.key == K_q) or (event.key == K_ESCAPE):
+                    exit()
+                if (event.key == K_e) or (event.key == K_RETURN):
+                    if langChooserAlive:
+                        self.langChooserAlive = False
+                    else:
+                        self.dialogAlive = False
+    
     def languageChooser(self):
-        dialogAlive = True
-        langChooserAlive = True
-        chosenLanguage = Languages.HU
+        self.chosenLanguage = Languages.HU
+        self.langChooserAlive = True
+        self.dialogAlive = True
+        
         langChooserBox = Rect((150,80), (740,570))
-        while dialogAlive:
-            if langChooserAlive:
-                self.drawer.fillWithBackgroundColor(self.screen, GameModes.MENU)
-                self.drawer.drawBox(langChooserBox, 3, self.screen, GameModes.MENU)
-                
+        while self.dialogAlive:
+            self.drawer.fillWithBackgroundColor(self.screen, GameModes.MENU)
+            self.drawer.drawBox(langChooserBox, 3, self.screen, GameModes.MENU)
+            if self.langChooserAlive:                
                 languages = [Languages.HU, Languages.EN, Languages.ROV]
                 textTypes = [CommonTextTypes.VAL_NYELV, CommonTextTypes.NYELV]
                 languageSurfaces = []
@@ -47,45 +82,19 @@ class Menu(object):
                 surfaceTypeEn = TextSurfaceTypes.NORMAL
                 surfaceTypeRov = TextSurfaceTypes.NORMAL
                 
-                if chosenLanguage == Languages.HU:
+                if self.chosenLanguage == Languages.HU:
                     surfaceTypeHu = TextSurfaceTypes.INVERSE
-                elif chosenLanguage == Languages.EN:
+                elif self.chosenLanguage == Languages.EN:
                     surfaceTypeEn = TextSurfaceTypes.INVERSE
-                elif chosenLanguage == Languages.ROV:
+                elif self.chosenLanguage == Languages.ROV:
                     surfaceTypeRov = TextSurfaceTypes.INVERSE
                     
                 self.screen.blit(languageSurfaces[0][1][0][surfaceTypeHu], (460, 400))
                 self.screen.blit(languageSurfaces[1][1][0][surfaceTypeEn], (460, 470))
                 self.screen.blit(languageSurfaces[2][1][0][surfaceTypeRov], (460, 560))
                 pygame.display.update()
-    
-                for event in pygame.event.get():
-                    if event.type == QUIT:
-                        exit()
-                    if event.type == KEYDOWN:
-                        if event.key == K_w or event.key == K_UP:
-                            if chosenLanguage == Languages.EN:
-                                chosenLanguage = Languages.HU
-                            elif chosenLanguage == Languages.HU:
-                                chosenLanguage = Languages.ROV
-                            elif chosenLanguage == Languages.ROV:
-                                chosenLanguage = Languages.EN
-                        if event.key == K_s or event.key == K_DOWN:
-                            if chosenLanguage == Languages.EN:
-                                chosenLanguage = Languages.ROV
-                            elif chosenLanguage == Languages.HU:
-                                chosenLanguage = Languages.EN
-                            elif chosenLanguage == Languages.ROV:
-                                chosenLanguage = Languages.HU
-                        if (event.key == K_q) or (event.key == K_ESCAPE):
-                            exit()
-                        if (event.key == K_e) or (event.key == K_RETURN):
-                            langChooserAlive = False
-            else:
-                self.drawer.fillWithBackgroundColor(self.screen, GameModes.MENU)
-                self.drawer.drawBox(langChooserBox, 3, self.screen, GameModes.MENU)
-                
-                surfaceIranyitas = self.textDrawer.getSurfaceArrayForCommonText(CommonTextTypes.IRANYITAS, GameModes.MENU, chosenLanguage, FontSizes.SMALL)
+            else:                
+                surfaceIranyitas = self.textDrawer.getSurfaceArrayForCommonText(CommonTextTypes.IRANYITAS, GameModes.MENU, self.chosenLanguage, FontSizes.SMALL)
                 
                 text_pos = 100
                 for surface in surfaceIranyitas:
@@ -93,14 +102,8 @@ class Menu(object):
                     text_pos += 50
                 pygame.display.update()
     
-                for event in pygame.event.get():
-                    if event.type == QUIT:
-                        exit()
-                    if event.type == KEYDOWN:
-                        if (event.key == K_q) or (event.key == K_ESCAPE):
-                            exit()
-                        if (event.key == K_e) or (event.key == K_RETURN):
-                            dialogAlive = False
-                            return chosenLanguage
+            self.eventLoop(self.langChooserAlive, self.chosenLanguage)
+            
+        return self.chosenLanguage
     
             
