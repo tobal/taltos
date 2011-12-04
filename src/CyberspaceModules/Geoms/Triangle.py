2ce97589e60dcdb56cf2e41486991e3248563d0a
Tobal <tobal17@gmail.com>
2011-12-04 17:23:12 +0000

    new Geoms package for geometries

---
+++ src/CyberspaceModules/Geoms/Triangle.py
@@ -0,0 +1,10 @@
+
+from CyberspaceModules.Geoms import Vertex
+
+class Triangle():
+	def __init__(self):
+		self.vertex = [None]*3
+		self.vertex[0] = Vertex()
+		self.vertex[1] = Vertex()
+		self.vertex[2] = Vertex()
+
---
+++ src/CyberspaceModules/Geoms/Vertex.py
@@ -0,0 +1,9 @@
+
+class Vertex():
+	def __init__(self):
+		self.x = 0
+		self.y = 0
+		self.z = 0
+		self.u = 0
+		self.v = 0
+
