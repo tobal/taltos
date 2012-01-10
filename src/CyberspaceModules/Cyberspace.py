57017cafd1c32d57a21122c53de8de4e99a4fc8e
Tobal <tobal17@gmail.com>
2012-01-10 21:01:17 +0000

    Basic grid complete

--- src/CyberspaceModules/Cyberspace.py
+++ src/CyberspaceModules/Cyberspace.py
@@ -3,17 +3,20 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from CommonModules.Screen.ImageHandler import ImageHandler
 
 class Cyberspace(object):
 
     def __init__(self):
         self.init()
         self.resize(1024, 768)
+        self.loadTexture()
 
     def init(self):
         glEnable(GL_DEPTH_TEST)
         glClearColor(0.0, 0.0, 0.0, 0.0)
         glShadeModel(GL_FLAT)
+        glEnable(GL_TEXTURE_2D)
         glEnable(GL_COLOR_MATERIAL)
         glEnable(GL_LIGHTING)
         glEnable(GL_LIGHT0)
@@ -27,14 +30,37 @@ class Cyberspace(object):
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
 
+    def loadTexture(self):
+        textureSurface = ImageHandler().loadImage("../Cyber/zold_negyzet")
+        textureData = pygame.image.tostring(textureSurface, "RGB", True)
+        self.texture = glGenTextures(1)
+
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
+        #glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+        width, height = textureSurface.get_rect().size
+        #glTexImage2D(GL_TEXTURE_2D, 0, 3,
+        #        width, height, 0,
+        #        GL_RGB, GL_UNSIGNED_BYTE, textureData)
+        gluBuild2DMipmaps(GL_TEXTURE_2D, 3,
+                width, height,
+                GL_RGB, GL_UNSIGNED_BYTE, textureData)
+
     def draw(self):
-        glBegin(GL_QUADS)
-        glColor(1.0, 0.0, 0.0)
-        glVertex(100.0, 100.0, -20.0)
-        glVertex(200.0, 100.0, -20.0)
-        glVertex(200.0, 200.0, -20.0)
-        glVertex(100.0, 200.0, -20.0)
-        glEnd()
+        for gridX in range(-400, 400, 4):
+            for gridZ in range(-400, 400, 4):
+                glBegin(GL_QUADS)
+                #glColor(1.0, 0.0, 0.0)
+                glTexCoord2f(0, 1)
+                glVertex(gridX, -3.0, gridZ)
+                glTexCoord2f(1, 1)
+                glVertex(gridX+4, -3.0, gridZ)
+                glTexCoord2f(1, 0)
+                glVertex(gridX+4, -3.0, gridZ+4)
+                glTexCoord2f(0, 0)
+                glVertex(gridX, -3.0, gridZ+4)
+                glEnd()
 
     def run(self):
         clock = pygame.time.Clock()
